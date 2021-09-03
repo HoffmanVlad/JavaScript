1180cdc12068b11c0f7e432bf1418c1f8885039d
vladislav <vladkalchenko08@gmail.com>
2021-09-03 14:12:38 +0300

    lesson01

---
+++ lesson01.js
@@ -0,0 +1,28 @@
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ];
+
+for(var i = 0; i < numbers.length; i++) {
+	if(numbers[i] > 50) {
+		console.log(numbers[i]);
+	}
+}
+
+var let = [];
+var str = "Backend As A Service";
+var first_let = "";
+
+str = str.split(' ');
+
+for(var i = 0; i < str.length; i++) {
+	letters.push(str[i][0]);
+}
+console.log(letters);
+
+for(i = 0; i <let.length; i++) {
+	first_let += let[i];
+}
+
+console.log(first_let);
+
+function Data(number) {
+	if(typeof(number) === "number")
+}
