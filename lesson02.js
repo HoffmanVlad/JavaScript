13b9d9c1f60e531bdbd56c883c9ef9f0aff1f89c
Vlad <84928134+HoffmanVlad@users.noreply.github.com>
2021-09-04 16:53:47 +0300

    Update lesson02.js

--- lesson02.js
+++ lesson02.js
@@ -13,8 +13,8 @@ let nums = [4,3,2,7,11,15];
 let target = 9;
 let counter = []
 
-for (let i = 0; i < nums.length; i++) {
-	for (let j = i + 1; j < nums.length; j++){
+for (var i = 0; i < nums.length; i++) {
+	for (var j = i + 1; j < nums.length; j++){
 		if (nums[i] + nums[j] === target) {
 			counter[0] = i;
 			counter[1] = j;
